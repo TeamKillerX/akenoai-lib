4c622d65b84c5bce00a2a10bc91766fea8a66d4b
Randy Dev <killerx@randydev.my.id>
2025-03-17 06:31:43 +0700

    Create federation.py

---
+++ examples/AkenoXAPI/federation.py
@@ -0,0 +1,30 @@
+import akenoai as js
+
+class Federation:
+    API_BASE = "https://randydev-ryu-js.hf.space/api/v2"
+    API_KEYS = "your_api_key"
+    
+    def __init__(self):
+        pass
+
+    async def federation_newfed(self, **data):
+        url = f"{self.API_BASE}/federation/newfed"
+        return await js.fetch(
+            url,
+            post=True,
+            json=data,
+            headers={"x-api-key": self.API_KEYS}
+        )
+    async def federation_subfed(self, **data):
+        url = f"{self.API_BASE}/federation/subfed"
+        return await js.fetch(
+            url,
+            post=True,
+            json=data,
+            headers={"x-api-key": self.API_KEYS}
+        )
+
+    async def federation_ban(self, **data):
+        pass
+
+# check out: https://t.me/RendyProjects/2537
