3921818d22289951a117bcd0741659e4dd81cbc0
Randy Dev <killerx@randydev.my.id>
2025-03-29 02:14:25 +0700

    _

--- akenoai/akeno.py
+++ akenoai/akeno.py
@@ -75,7 +75,7 @@ class ScraperProxy(BaseModel):
 
 @dataclass
 class BaseDev:
-    public_url: str
+    public_url: str = field(default="")
     obj: Box = field(default_factory=Box)
 
     def _get_random_from_channel(self, link: str = None):
