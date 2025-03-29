50979061841114852e1e8813561ff683f5cc0164
Randy Dev <killerx@randydev.my.id>
2025-03-29 19:56:34 +0700

    _

--- akenoai/akeno.py
+++ akenoai/akeno.py
@@ -158,7 +158,7 @@ class BaseDev:
             return [a['href'] for a in soup.find_all('a', href=True)] if x.extract_all_hrefs else []
         if x.use_proxy_mode:
             proxies = {
-                "https": f"scraperapi:{x.api_key}@proxy-server.scraperapi.com:{x.port}"
+                "https": f"https://scraperapi:{x.api_key}@proxy-server.scraperapi.com:{x.port}"
             }
             frspon = requests.post(
                 x.url,
