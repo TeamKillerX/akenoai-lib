92040c4a6eeaad05188b36a7949e232a30c99297
Randy Dev <killerx@randydev.my.id>
2025-04-01 02:25:01 +0700

    _

--- akenoai/types/__init__.py
+++ akenoai/types/__init__.py
@@ -72,7 +72,7 @@ class HeaderOptions(BaseModel):
     headers_update: Optional[dict] = None
 
 class EditCustomOpenai(BaseModel):
-    logo_url: Optional[str] = "https://github-production-user-asset-6210df.s3.amazonaws.com/90479255/289277800-f26513f7-cdf4-44ee-9a08-f6b27e6b99f7.jpg"
+    logo_url: Optional[str] = None
     title: Optional[str] = "AkenoX Demo API"
     version: Optional[str] = "1.0.0"
     summary: Optional[str] = "Use It Only For Personal Projects"
